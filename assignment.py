d454f35f22808f8a1e063a7e5d30ad6d3779ec87
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-07-26 13:01:39 +0600

    student class

--- assignment.py
+++ assignment.py
@@ -1,6 +1,27 @@
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/YIAHIAAHMED/assignment_02.git
-# git push -u origin main
+#1. Create the StudentDatabase class
+
+#Solution Code:
+class StudentDatabase:
+        student_list = []
+
+        @classmethod
+        def add_student(cls, student):
+            cls.student_list.append(student)
+
+
+
+#2. Create the Student class
+
+#Solution Code:
+class Student:
+    def __init__(self, student_id, name, department, is_enrolled):
+        self.student_id = student_id
+        self.name = name
+        self.department = department
+        self.is_enrolled = is_enrolled
+
+
+#3. Initialize Student Object
+
+#Solution Code:
+
