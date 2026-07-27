bb2eff6c46cacf40874b3047db3e9916b28bb470
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-07-27 18:53:03 +0600

    m_system

--- assignment.py
+++ assignment.py
@@ -121,3 +121,26 @@ student2.view_student_info()
 #7. Menu System
 
 #Solution Code:
+while True:
+    print('\n-------------------------------\n')
+    print('1. View All Students.')
+    print('2. Enroll Student.')
+    print('3. Drop Student.')
+    print('4. Exit.')
+    option = int(input('Enter your option: '))
+    if option == 1:
+        print('\n-------------------------------\n')
+        print('View All Students.')
+
+    elif option == 2:
+        print('Enroll Student.')
+
+    elif option == 3:
+        print('Drop Student.')
+
+    elif option == 4:
+        print('Exit.')
+        break
+
+    else:
+        print('wrong option. choose again\n')  
--- menu_system.py
+++ menu_system.py
@@ -1,38 +1,23 @@
-class Student:
-    def __init__(self, student_id, name, department, is_enrolled):
-        self.student_id = student_id
-        self.name = name
-        self.department = department
-        self.is_enrolled = is_enrolled
+while True:
+    print('\n-------------------------------\n')
+    print('1. View All Students.')
+    print('2. Enroll Student.')
+    print('3. Drop Student.')
+    print('4. Exit.')
+    option = int(input('Enter your option: '))
+    if option == 1:
+        print('\n-------------------------------\n')
+        print('View All Students.')
 
+    elif option == 2:
+        print('Enroll Student.')
 
-    def view_student_info(self):
-            print(f'Student Id: {self.student_id}')
-            print(f'Name: {self.name}')
-            print(f'Department: {self.department}')
-            print(f'Is_enrolled: {self.is_enrolled}')
+    elif option == 3:
+        print('Drop Student.')
 
-    def enroll_student(self):
-            if not self.is_enrolled:
-                self.is_enrolled = True
-                print(f'{self.name} has been enrolled successfully.')
-            else:
-                print(f'{self.name} is already enrolled.')
+    elif option == 4:
+        print('Exit.')
+        break
 
-    def drop_student(self):
-                self.is_enrolled = False
-                print(f'{self.name} has dropped out.')
-
-class StudentDatabase:
-    student_list = []
-
-    @classmethod
-    def add_student(cls, student):
-        cls.student_list.append(student)
-        
-
-student1 = Student(101, 'Yiahia', 'CSE', True)
-student2 = Student(102, 'Ahmed', 'ME', False)
-
-student1.view_student_info()
-student2.view_student_info()
+    else:
+        print('wrong option. choose again\n')  
